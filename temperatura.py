891cf5e62e661365a121bed5eb640b1d13c7cd22
Irismenia <irismeniaalviarez@colegioguanenta.edu.co>
2022-04-21 18:31:26 -0500

    desarrollo: creacion diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+#imput 
+C=int(input("digitel el valor de los grados celcius: "))
+
+
+# precesing
+k=C+273.15
+f=1.8*C+32
+
+#output
+print(str(C) + " grados celcius " + " en kelvin es " + str(k) + " y en farenheit " + str(f))
